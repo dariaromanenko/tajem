b0943345a40308e2194755747ff767a0d7377680
dariaromanenko <spavak.darya.v@gmail.com>
2019-04-14 21:33:46 +0300

    second commit

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+$(function(){
+
+    // $('.menu__btn').click(function(){
+    //     $('.menu ul').slideToggle();
+    // });
+
+    // $('.menu__btn').click(function() {
+    //     $('.menu__list').toggleClass('shown');
+    // });
+
+
+    $('.slick-slider-menu').slick({
+      dots: true,
+        arrows: false
+
+    });
+
+
+    // $('.slider-for').slick({
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     arrows: false,
+    //     fade: true,
+    //     asNavFor: '.slider-nav'
+    // });
+    // $('.slider-nav').slick({
+    //     slidesToShow: 3,
+    //     slidesToScroll: 1,
+    //     asNavFor: '.slider-for',
+    //     dots: true,
+    //     centerMode: true,
+    //     focusOnSelect: true
+    // });
+
+});
